864d8b186e94fecf0d59219296ac12c07f8b282a
betsaleel yoto <betsaleelyoto@gmail.com>
2024-02-03 00:34:26 +0200

    Version du 3fev024 00:34

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { Mesusers,IdUser } = require("../controllers/usersControleur.js");
-
+const { Mesusers, IdUser } = require("../controllers/usersControleur.js");
 
 router.get("/", Mesusers);
-router.get("/:id",IdUser);
+router.get("/:id", IdUser);
+router.post("");
+router.delete("");
+router.put("");
+
 
 module.exports = router;
